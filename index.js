486ebdab9f216b0d86972f492c2953059248113d
zhangshikui <zhangshikui@winupon.com>
2024-03-07 17:03:12 +0800

    init

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 /*
  * @Date: 2024-03-07 15:25:38
  * @LastEditors: zhangsk
- * @LastEditTime: 2024-03-07 15:26:17
- * @FilePath: \npm\check-type\index.js
+ * @LastEditTime: 2024-03-07 17:02:57
+ * @FilePath: \zsk6-check-type\index.js
  * @Label: Do not edit
  */
 export default function checkType() {
-  return "这个一个index文件";
+  return "这个一个index文件2";
 }
